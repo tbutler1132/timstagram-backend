d67ed32594c835f1805f7f8c37c67bd301964777
github username <tbutler1132@gmail.com>
2021-03-17 11:13:00 -0400

    delete almost completely working

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -9,4 +9,26 @@ class PicturesController < ApplicationController
     def show
         picture = Picture.find(params[:id])
     end
+
+    def create
+        @picture = Picture.create!(picture_params)
+        if @picture.save!
+          render json: @picture, status: :created
+        else
+          render json: { error: 'failed to create picture' }, status: :not_acceptable
+        end
+    end
+
+    def destroy
+        picture = Picture.find(params[:id])
+        picture.destroy
+        render json: {}
+    end
+
+    private 
+
+    def picture_params
+        params.require(:picture).permit(:photo_url, :Caption, :user_id)
+    end
+
 end
