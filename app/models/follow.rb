151b1a68bf6aca8c627db514595a5c918c0ccd7d
github username <tbutler1132@gmail.com>
2021-03-26 13:25:48 -0400

    validate followers added

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,4 +1,6 @@
 class Follow < ApplicationRecord
+    validates_uniqueness_of :follower_id, :scope => [:followee_id]
+
     belongs_to :follower, class_name: 'User'
     belongs_to :followee, class_name: 'User'
 end
